7de332b0f1e5fe9349c6371e81362d2485a6f3a9
AugustoMachado01 <augustomachadochieno01@gmail.com>
2024-10-25 17:06:01 -0400

    first commit

---
+++ db/conn.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("nodemvc", "root", "augustomachado01", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+try {
+  sequelize.authenticate();
+
+  console.log("conectou ao banco de dados MYSQL");
+} catch (error) {
+  console.log(`Nao foi possivel conectar ${error}`);
+}
